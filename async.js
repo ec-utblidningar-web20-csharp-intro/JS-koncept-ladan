42f6e91ee397894cc1fea4de5f85e134df9dedd7
Björn Strömberg <bjorn.stromberg@codic.se>
2021-03-02 16:01:53 +0100

    Det sissta om loopar

--- async.js
+++ async.js
@@ -2,20 +2,23 @@
 // <script defer="true" src=...>
 
 document.querySelector("#async #pikachu-btn").onclick = async function (event) {
-    let url = new URL("https://pokeapi.co/api/v2/pokemon/pikachu");
-    
-    // Hämta data från pokemon web-api:n 
-    let response = await fetch(url);
-    console.log("we have an answer from the server");
-    let pokemon = await response.json();
-    console.log("we have pikachu objekt!");
+  let url = new URL("https://pokeapi.co/api/v2/pokemon/pikachu");
 
-    // Ändra i dom när pokemonen kommit
-    let parent = document.querySelector("#async");
-    parent.insertAdjacentHTML('beforeend', `<h3>Pokemon ${pokemon.name}</h3>`);
-    
-    let urlArray = Object.values(pokemon.sprites);
-    parent.insertAdjacentHTML('beforeend', `<img src=${pokemon.sprites.front_default}></img>`);
+  // Hämta data från pokemon web-api:n
+  let response = await fetch(url);
+  console.log("we have an answer from the server");
+  let pokemon = await response.json();
+  console.log("we have pikachu objekt!");
+
+  // Ändra i dom när pokemonen kommit
+  let parent = document.querySelector("#async");
+  parent.insertAdjacentHTML("beforeend", `<h3>Pokemon ${pokemon.name}</h3>`);
+
+  let urlArray = Object.values(pokemon.sprites);
+  parent.insertAdjacentHTML(
+    "beforeend",
+    `<img src=${pokemon.sprites.front_default}></img>`
+  );
 };
 
 console.log("main.js has finished running");
